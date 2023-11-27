1882e30bbdce99d93207c60ce263d2558d2ef31c
amal thomas <amalthomas286@gmail.com>
2023-11-27 16:37:17 +0530

    first commit

--- yolo1/v8.py
+++ yolo1/v8.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Loading model
-model = YOLO("yolov8n.yaml")  
-model.train(data="yolo1\conf.yaml", epochs=90)
+model = YOLO("yolov8n.pt")  
+model.train(data="yolo1\conf.yaml", epochs=100)
 
 
